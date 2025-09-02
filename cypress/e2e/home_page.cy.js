c3e974e219a6541d76c242c53eecab592eb0f237
LindaOunissi <77611504+Linda-ounissi@users.noreply.github.com>
2025-09-02 23:16:53 +0200

    Ajout test passant

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -4,3 +4,8 @@ describe('The Home Page', () => {
   })
 })
 
+it('uses custom text for the button label', () => {
+  cy.visit('/')
+  // Assert that a button component has the correct text
+  cy.get('button').should('contains.text', 'See All Tools')
+})
