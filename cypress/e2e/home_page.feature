016fb7bd9a540dbb4b8e4de3fba2177730c0b09a
LindaOunissi <77611504+Linda-ounissi@users.noreply.github.com>
2025-09-03 01:35:02 +0200

    upgrade version + plugin cucumber

---
+++ cypress/e2e/home_page.feature
@@ -0,0 +1,4 @@
+Feature: elements home page
+  Scenario: visiting the frontpage
+    Given I visit my app pastry tool
+    Then I should see a search button to see all tool
---
+++ cypress/e2e/home_page.js
@@ -0,0 +1,11 @@
+const { Given, When, Then } = require("@badeball/cypress-cucumber-preprocessor");
+
+Given("I visit my app pastry tool", () => {
+  cy.visit("http://localhost:3000");
+});
+
+Then("I should see a search button to see all tool", () => {
+  cy.get("button")
+    .should('contain.text', 'See All Tools');
+
+});
