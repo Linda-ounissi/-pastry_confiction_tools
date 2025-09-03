11b5f371d9c3b46390eaa8bd1ead2e8adc9492d3
LindaOunissi <77611504+Linda-ounissi@users.noreply.github.com>
2025-09-03 17:20:59 +0200

    mis à jour scenario

--- cypress/e2e/home_page.feature
+++ cypress/e2e/home_page.feature
@@ -1,4 +1,7 @@
-Feature: elements home page
-  Scenario: visiting the frontpage
+Feature: Recherche d'outils
+  Scenario: Recherche de livre de recette
     Given I visit my app pastry tool
-    Then I should see a search button to see all tool
+    And Je dois voir le bouton to see all tool
+    And Je clique sur le lien pour voir tous les outils
+    And Je saisis un mot-clé de recherche macaron
+    Then je dois voir une suggestion de livre de recette  pour macaron
--- cypress/support/step_definitions/home_page.js
+++ cypress/support/step_definitions/home_page.js
@@ -4,9 +4,22 @@ Given("I visit my app pastry tool", () => {
   cy.visit("http://localhost:3000");
 });
 
-Then("I should see a search button to see all tool", () => {
+Given("Je dois voir le bouton to see all tool", () => {
   cy.get("button")
     .should('contain.text', 'See All Tools');
+});
+
+Given("Je clique sur le lien pour voir tous les outils",()=>{
+  cy.get('.bottom').click()
 
 });
+Given("Je saisis un mot-clé de recherche macaron",()=>{
+  cy.get('#query').type('macaron')
+});
+Then("je dois voir une suggestion de livre de recette  pour macaron",()=>{
+  cy.get('input[name="commit"]').click()
+});
+
+
+
 
